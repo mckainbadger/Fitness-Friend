a30bdea7e5f4fd07a6f3322bf464e6a4bb2b9cc8
mckainbadger <132297616+mckainbadger@users.noreply.github.com>
2023-08-02 13:36:47 -0400

    created all DB tables

--- api/migrations/20230801172123_workouts.js
+++ api/migrations/20230801172123_workouts.js
@@ -3,10 +3,12 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
+    return knex.schema.createTable('workouts', function(table) {
     table.increments('id').primary();
     table.integer('userId').notNullable().references('id').inTable('users').onDelete('CASCADE');
     table.datetime('dateTime').notNullable();
     table.timestamp('updated_at').defaultTo(knex.fn.now())
+    })
 };
 
 /**
