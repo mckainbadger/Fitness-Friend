bb19e7606e9c18dfc5e478492bf57f7d24add30f
mckainbadger <132297616+mckainbadger@users.noreply.github.com>
2023-08-03 19:35:08 -0400

    added routers/services/controller for workouts

--- ui/src/utility/api.js
+++ ui/src/utility/api.js
@@ -1,12 +1,12 @@
 
 export const getAllExercises = async () => {
-    const response = await fetch(`${baseUrl}/crimes/`)
+    const response = await fetch(`${baseUrl}/exercises/`)
     const responseData = await response.json()
     return responseData
   }
 
   export const createNewWorkout = async (reportData) => {
-    const response = await fetch(`${baseUrl}/reports/new`, {
+    const response = await fetch(`${baseUrl}/workout/new`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(reportData),
