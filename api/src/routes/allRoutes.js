fb8585bfb3e6760b2f4c88a4c4fce3810b3c31b8
mckainbadger <132297616+mckainbadger@users.noreply.github.com>
2023-08-03 19:35:35 -0400

    added controller/services/router to workouts

--- api/src/routes/allRoutes.js
+++ api/src/routes/allRoutes.js
@@ -3,6 +3,7 @@ const { Router } = require('express')
 // import routes
 const root = require('./root/router')
 const users = require('./users/router')
+const exerciseOptions  = require('./exerciseOptions/router')
 
 // create a new Router instance
 const allRouters = new Router()
@@ -10,6 +11,8 @@ const allRouters = new Router()
 // create base routes
 allRouters.use('/', root)
 allRouters.use('/users', users)
+allRouters.use('/exerciseOptions', exerciseOptions)
+
 
 // exporting router
 module.exports = allRouters
