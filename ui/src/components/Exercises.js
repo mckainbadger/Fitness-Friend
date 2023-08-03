d81841f5168fcf0da427909b27f75dae8ccac940
mckainbadger <132297616+mckainbadger@users.noreply.github.com>
2023-08-03 08:19:11 -0400

    displayin more data

--- ui/src/components/Exercises.js
+++ ui/src/components/Exercises.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import { Pagination } from '@mui/material'
 
 const Exercises = () => {
   return <div>ExerciseDetail</div>
--- ui/src/components/HorizontalScrollBar.js
+++ ui/src/components/HorizontalScrollBar.js
@@ -1,10 +1,36 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import {ScrollMenu, VisibilityContext} from "react-horizontal-scrolling-menu";
 import BodyPart from "./BodyPart";
+import RightArrowIcon from "../assets/icons/right-arrow.png";
+import LeftArrowIcon from "../assets/icons/left-arrow.png";
+import { useContext } from "react";
+
+const LeftArrow = () => {
+  const { scrollPrev } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollPrev()} className="right-arrow">
+      <img src={LeftArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+
+const RightArrow = () => {
+  const { scrollNext } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollNext()} className="left-arrow">
+      <img src={RightArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+
+
 
 const HorizontalScrollBar = ({ data, bodyParts, setBodyPart }) => {
   return (
-    <div>
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
       {data.map((item) => (
         <Box
           key={item.id || item}
@@ -15,7 +41,7 @@ const HorizontalScrollBar = ({ data, bodyParts, setBodyPart }) => {
            < BodyPart item={item} bodyParts = {bodyParts} setBodyPart={setBodyPart} />
         </Box>
       ))}
-    </div>
+    </ScrollMenu>
   );
 };
 
