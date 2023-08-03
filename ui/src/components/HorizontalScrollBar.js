7bf9d1db71f5b588702562ea21be551c7ea886ce
mckainbadger <132297616+mckainbadger@users.noreply.github.com>
2023-08-03 11:30:38 -0400

    styling

--- ui/src/components/HorizontalScrollBar.js
+++ ui/src/components/HorizontalScrollBar.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
-import {ScrollMenu, VisibilityContext} from "react-horizontal-scrolling-menu";
+import { Box, Typography, Grid } from "@mui/material";
+import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
 import BodyPart from "./BodyPart";
 import RightArrowIcon from "../assets/icons/right-arrow.png";
 import LeftArrowIcon from "../assets/icons/left-arrow.png";
@@ -26,21 +26,29 @@ const RightArrow = () => {
   );
 };
 
+const HorizontalScrollBar = ({ data, bodyParts }) => {
+ 
+  const setBodyPart = () =>{
+  }
 
-
-const HorizontalScrollBar = ({ data, bodyParts, setBodyPart }) => {
   return (
     <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-      {data.map((item) => (
-        <Box
-          key={item.id || item}
-          itemId={item.id || item}
-          title={item.id || item}
-          m="0 40px"
-        >
-           < BodyPart item={item} bodyParts = {bodyParts} setBodyPart={setBodyPart} />
-        </Box>
-      ))}
+      <Grid container justifyContent='center' alignItems='center'>
+        {data.map((item) => (
+          <Box
+            key={item.id || item}
+            itemId={item.id || item}
+            title={item.id || item}
+            m="0 40px"
+          >
+            <BodyPart
+              item={item}
+              bodyParts={bodyParts}
+              setBodyPart={setBodyPart}
+            />
+          </Box>
+        ))}
+      </Grid>
     </ScrollMenu>
   );
 };
