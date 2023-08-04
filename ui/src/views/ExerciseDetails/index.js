cd3710fa4a2f9381b9008afb10eb88f564b0d75b
mckainbadger <132297616+mckainbadger@users.noreply.github.com>
2023-08-03 21:25:09 -0400

    finished individual detail page

--- ui/src/views/ExerciseDetails/index.js
+++ ui/src/views/ExerciseDetails/index.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box} from '@mui/material'
+import Detail from '../../components/Detail'
+import { exerciseOptions, fetchData } from '../../utility/fetchData'
 
 const ExerciseDetail = () => {
-  return <div>ExerciseDetail</div>
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const {id} = useParams()
+
+  useEffect (() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com'
+
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`,
+      exerciseOptions);
+      setExerciseDetail(exerciseDetailData)
+    }
+    fetchExercisesData()
+  }, [id]);
+  return(
+   <Box>
+  <Detail exerciseDetail={exerciseDetail}/>
+  </Box>
+  )
 }
 
 export default ExerciseDetail
